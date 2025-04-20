4aade92bd8a150489fc5498b185b5aa2d179c431
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-04-20 19:40:25 +0530

    Second Question

---
+++ 2variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare two integer variables
+    int num1 = 10; // First number
+    int num2 = 20; // Second number
+
+    // Calculate their sum
+    int sum = num1 + num2;
+
+    // Display the result
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}

1b4e45dd3ec1b6fab1782f7f666e33898e011a54
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-05-05 07:21:15 +0530

    Update

--- 31arrayminval2.cpp
+++ 31arrayminval2.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(){
     int smallest[5]={2,3,4,5,6};
-    int max=INT_MAX;
+    int minval=INT_MAX;
     for(int i=0;i<5;i++){
-        if(smallest[i]<max){
-        max=smallest[i];
+        if(smallest[i]<minval){
+        minval=smallest[i];
         
     }
   
     }
-    cout<<max<<endl;
+    cout<<minval<<endl;
     return 0;
 }

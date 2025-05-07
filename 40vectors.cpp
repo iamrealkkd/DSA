e0f9aa7c2bb02684b728fa30d3630dd607297f1a
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-05-07 06:20:41 +0530

    Vectors

---
+++ 40vectors.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+int main(){
+    vector<int>num={1,2,3,4,5};
+    cout<<num[0]<<endl;
+    cout<<num[1]<<endl; 
+    cout<<num[2]<<endl; 
+    cout<<num[3]<<endl; 
+    cout<<num[4]<<endl;
+return 0;
+}

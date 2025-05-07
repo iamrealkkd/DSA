964a4b389af83ad58963d8a404fcb4ce0c68e409
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-05-07 06:34:54 +0530

    Vectors

---
+++ 42vectors.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<float>num={1.2,2.6,3.5,4.8,5.9};
+    for(float j:num){
+        cout<<j<<endl;
+    }
+return 0;  
+}

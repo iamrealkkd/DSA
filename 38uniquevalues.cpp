799d6b907f9d6ef0c93b69103c03c5756d403e72
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-06-15 20:59:07 +0530

    Array

---
+++ 38uniquevalues.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Function to print unique numbers in the array
+void printUniqueNumbers(int arr[], int n) {
+    cout << "Unique numbers are: ";
+    for (int i = 0; i < n; i++) {
+        bool isUnique = true;
+        for (int j = 0; j < n; j++) {
+            if (i != j && arr[i] == arr[j]) {
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printUniqueNumbers(arr, n);
+
+    return 0;
+}
---
+++ 39intersection.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Function to print the intersection of two arrays
+void printIntersection(int arr1[], int n1, int arr2[], int n2) {
+    cout << "Intersection of the two arrays: ";
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (arr1[i] == arr2[j]) {
+                cout << arr1[i] << " ";
+                arr2[j] = -1; // Mark as visited to avoid duplicates
+                break;
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {4, 5, 6, 7, 8};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    printIntersection(arr1, n1, arr2, n2);
+
+    return 0;
+}

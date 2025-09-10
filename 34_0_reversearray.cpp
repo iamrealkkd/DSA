d3ea3a059a4834653fc0f231cd2bb798577c81e6
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-09-10 08:33:13 +0530

    Re run

--- 34_0_reversearray.cpp
+++ 34_0_reversearray.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {1, 2, 3, 4, 5};\
+    int arr[5] = {1, 2, 3, 4, 5};
     int size=5;
     int start = 0;
     int end = size-1;
     while(start<end){
-swap(arr[start], arr[end]);
+        swap(arr[start], arr[end]);
         start++;
         end--;
        

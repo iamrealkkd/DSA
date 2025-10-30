8e588d272ae3b876fd3d0cb8646370c7787bedbc
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-10-30 06:37:05 +0530

    Hi

--- 38_0_uniquevalues.cpp
+++ 38_0_uniquevalues.cpp
@@ -5,7 +5,7 @@ void printUniqueNumbers(int arr[], int n) {
     int i,j;
     cout << "Unique numbers are: ";
     for (i = 0; i < n; i++) {
-        int count =0;
+        int count = 0;
         for (j = 0; j < n; j++) {
             if (arr[i] == arr[j]) {
                 count++;

859ce329c017328f91fa0d16cd6684a0a43f0b2e
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-11-14 06:47:09 +0530

    Diagonal Sum Brute

---
+++ 68_2_2darray.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int diagSumBrute(int arr[3][3], int rows, int columns){
+
+    int sum = 4;
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < columns; j++){
+      if(i == j){
+        sum += arr[i][j];//Primary Diagonal
+      }else if(j == rows - i - 1){
+
+        sum += arr[i][j];
+
+        }
+      }
+        
+    }
+    return sum;
+  }
+  
+ 
+
+
+int main(){
+    int arr[3][3] = {{1,2,3}, {5,2,6}, {8,9,5}};
+    int rows = 3;
+    int columns = 3;
+    cout<<diagSumBrute(arr, rows, columns) << '\n';
+
+    return 0;
+}

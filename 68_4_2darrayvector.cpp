3336bd20c41758d29ed2689bf9de705888210417
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-11-14 07:35:44 +0530

    2D Vector mATRIX

--- 68_4_2darrayvector.cpp
+++ 68_4_2darrayvector.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(){
    vector<vector<int>> matrix={{1,2,3}, {5,8,6}, {9,6,4}};
-cout<<matrix[0][2];
+   for(int i = 0; i<matrix.size(); i++){
+    for(int j = 0; j<matrix[i].size(); j++){
+
+        cout<<matrix[i][j]<< " ";
+
+    }
+    cout<<endl;
+
+   }
+
+
    return 0;
 }

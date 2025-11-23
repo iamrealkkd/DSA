cfce677cf848713de1975e471ab60a6724cfb5d7
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-11-23 10:42:50 +0530

    Sub Array Sum

---
+++ 75_1_subarrsum_560_optimised.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        int count = 0;
+        vector<int> prefixSum(n, 0);
+
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        unordered_map<int, int> m;
+
+        for (int j = 0; j < n; j++) {
+
+            if (prefixSum[j] == k) count++;
+
+            int val = prefixSum[j] - k;
+
+            if (m.find(val) != m.end()) {
+                count += m[val];
+            }
+
+            if (m.find(prefixSum[j]) == m.end()) {
+                m[prefixSum[j]] = 0;
+            }
+
+            m[prefixSum[j]]++;
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    vector<int> arr = {1, 2, 3};
+    int k = 3;
+    Solution obj;
+    cout << obj.subarraySum(arr, k) << endl;
+    return 0;
+}

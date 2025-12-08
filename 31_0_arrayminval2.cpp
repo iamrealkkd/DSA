ccbba9d4cfa7a43921805600bf799d704d75e1b1
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-08 18:14:21 +0530

    Designing

--- 31_0_arrayminval2.cpp
+++ 31_0_arrayminval2.cpp
@@ -3,17 +3,16 @@
 using namespace std;
 
 int main(){
-    int i;
-    int smallest[5]={2,3,4,5,6};
-    int minval=INT_MAX;
-    for(i=0;i<5;i++){
-        if(smallest[i]<minval){
-        minval=smallest[i];
-        
+    int smallest[5] = {2, 3, 4, 5, 6};
+    int minval = INT_MAX;
+
+    for(int i = 0; i < 5; i++){
+
+        if(smallest[i] < minval){
+        minval = smallest[i];  
     }
   
     }
-    cout<<minval<<endl;
-    cout<<smallest[i]<<endl;
+    cout << minval << endl;
     return 0;
 }

377ac616e514b0c1592e5a191ff242e9022c7844
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-08 18:10:03 +0530

    Designing

--- 30_0_arraymaxvalue1.cpp
+++ 30_0_arraymaxvalue1.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i;
-    int highest[5]={2,3,4,5,6};
-    int max=highest[0];
-    for(i=0;i<5;i++){
-        if(highest[i]>max){
-        max=highest[i];
-        
+
+    int highest[5] = {2, 3, 4, 5, 6};
+    int max = highest[0];
+
+    for(int i = 0; i < 5; i++){
+
+        if(highest[i] > max){
+        max = highest[i];
     }
   
     }
-    cout<<max<<endl;\
-    cout<<highest[i]<<endl;
+    cout << max << endl;
     return 0;
 }

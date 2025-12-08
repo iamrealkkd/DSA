5a52cee4567b7863c2467dacf6ef9331dbe67954
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-08 18:30:09 +0530

    Design

--- 43_0_leetvec_136.cpp
+++ 43_0_leetvec_136.cpp
@@ -2,11 +2,13 @@
 #include<vector>
 using namespace std;
 int main(){
-    int ans=0;
-    vector<int> num={4,1,2,1,2};
+    int ans = 0;
+    vector<int> num = {4, 1, 2, 1, 2};
+
     for (int i :num){
         ans^=i;
     }
-    cout<<ans<<endl;
+
+    cout << ans << endl;
     
 }

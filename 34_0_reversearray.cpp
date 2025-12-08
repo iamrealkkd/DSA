af0842cfcb0a30024115f2dbe586d1418b75eb8f
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-08 18:26:48 +0530

    Design

--- 34_0_reversearray.cpp
+++ 34_0_reversearray.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {1, 2, 3, 4, 5};
-    int size=5;
+
+    int arr[6] = {1, 2, 3, 4, 5, 8};
+    int size = 6;
     int start = 0;
     int end = size-1;
-    while(start<end){
+
+    while(start <= end){
+        
         swap(arr[start], arr[end]);
         start++;
         end--;
-       
     }
    
-    cout<<"Reversed array is: "<<endl;
-    for(int i=0; i<size; i++){
-        cout<<arr[i]<<" ";
+    cout <<"Reversed array is: "<< endl;
+    for(int i = 0; i < size; i++){
+        cout << arr[i]<<" ";
     }   
+
     return 0;
 }

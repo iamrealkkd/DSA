80201a53dfbb1c359e2804f3ee72158a6611268e
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2025-12-08 18:20:05 +0530

    Design

--- 32_0_passbyref.cpp
+++ 32_0_passbyref.cpp
@@ -1,27 +1,26 @@
 #include<iostream>
 using namespace std;
-void chngearr(int arr[], int size){
-    int i;
-    cout<<"In function before changing the array values: "<<endl;
-    for(i=0;i<size;i++){
-        arr[i]=5*arr[i];
+void changeArray(int arr[], int size){
+
+    cout << "In function before changing the array values: " << endl;
+
+    for(int i = 0; i < size; i++){
+        arr[i] = 5*arr[i];
     }
 }
 
-
-
-
-
-
-
-
 int main(){
-    int arr[5]={1,2,3,4,5};
-    chngearr(arr,5);
+
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    changeArray(arr,5);
+
     cout<<"In main function after changing the array values: "<<endl;
-    for(int i=0;i<5;i++){
-        cout<<arr[i]<<" ";
+
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] <<" ";
     }
+
     cout<<endl;
 
 }

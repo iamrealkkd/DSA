4287c864321dd2855962402e13bcffa513e570e7
iamrealkkd <kkrishna.bga@gmail.com>
2026-03-27 12:45:28 +0530

    Kth Symbol in Grammar

--- 90_0_kth_symbol_in_grammar.cpp
+++ 90_0_kth_symbol_in_grammar.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if (n == 1) return 0;
+         if (n == 1 && k == 1)
+            return 0;
 
-        if (k % 2 == 1)
-            return kthGrammar(n - 1, (k + 1) / 2);
-        else
-            return 1 - kthGrammar(n - 1, k / 2);
+        int mid = pow(2, n - 1) / 2;
+
+        if (k <= mid) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return !kthGrammar(n - 1, k - mid);
+        }
     }
 };
 
@@ -20,9 +25,7 @@ int main() {
     cout << "Enter n and k: ";
     cin >> n >> k;
 
-    int result = obj.kthGrammar(n, k);
-
-    cout << "Kth Grammar value is: " << result << endl;
+    cout << "Kth Grammar value: " << obj.kthGrammar(n, k) << endl;
 
     return 0;
 }

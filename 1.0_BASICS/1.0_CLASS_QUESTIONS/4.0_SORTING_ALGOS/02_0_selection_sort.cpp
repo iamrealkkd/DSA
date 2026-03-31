b9026f7554bf4b69271444399792d56d50ca990b
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-03-31 07:16:34 +0530

    Sorting Algos

---
+++ 1.0_BASICS/1.0_CLASS_QUESTIONS/4.0_SORTING_ALGOS/02_0_selection_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {5, 2, 9, 1, 6};
+    int n = 5;
+
+    for(int i = 0; i < n-1; i++){
+
+        int minIndex = i;
+
+        for(int j = i+1; j < n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+
+    cout << "Sorted Array:\n";
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

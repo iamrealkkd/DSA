335b95f38b659a8acaf33cfc96b55c847bc1c7f9
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-04-13 09:22:44 +0530

    Recursion Qstn Updation

--- 5.0_RECURSION_CONCEPTS/1.0_CLASS_QUESTIONS/07_0_print_descending.cpp
+++ 5.0_RECURSION_CONCEPTS/1.0_CLASS_QUESTIONS/07_0_print_descending.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    void print(int n){
-      if(n == 1){
-        cout << 1 << " ";
-        return;
-      }
-      cout << n << " ";
-      print(n-1);
-      
+void print(int n){
+    if(n == 1){
+      cout << 1 << " ";
+      return;
+    }
+    cout << n << " ";
+    print(n-1);  
     }
 int main(){
-    print(5);
+    print(7);
 }

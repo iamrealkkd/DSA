ea5a1c186b6d51c4d4ae956e080f8d145e4451b4
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-05 13:21:28 +0530

    Perfect Sum Updated

--- 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/04_0_perfect_sum_problem_gfg.cpp
+++ 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/04_0_perfect_sum_problem_gfg.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution
 {
 public:
@@ -8,22 +11,22 @@ public:
     {
         int n = arr.size();
 
-        vector<vector<int>> t(n + 1, vector<int>(target + 1));
+        vector<vector<int>> t(n + 1, vector<int>(target + 1, 0));
 
         // Initialization
-
         for (int i = 0; i <= n; i++)
-            t[i][0] = true;
+            t[i][0] = 1;
 
         for (int j = 1; j <= target; j++)
-            t[0][j] = false;
+            t[0][j] = 0;
+
         // DP
         for (int i = 1; i <= n; i++)
         {
             for (int j = 0; j <= target; j++)
             {
                 if (arr[i - 1] <= j)
-                    t[i][j] = (t[i - 1][j - arr[i - 1]] + t[i - 1][j]);
+                    t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j];
                 else
                     t[i][j] = t[i - 1][j];
             }

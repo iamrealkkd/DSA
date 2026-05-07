2c921ce008ab0240d8c5e797f6ad14d533925865
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-07 08:07:55 +0530

    Knapsack Class Based Code

--- 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/01_2_knapsack_bottom_up_approach.cpp
+++ 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/01_2_knapsack_bottom_up_approach.cpp
@@ -1,47 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int knapsack(int wt[], int val[], int W, int n)
+class Solution
 {
-  int t[n + 1][W + 1];
-
-  // initialize
-  // n, W ki jgh i and j
-  for (int i = 0; i <= n; i++)
+public:
+  int knapsack(vector<int> &wt, vector<int> &val, int W, int n)
   {
-    for (int j = 0; j <= W; j++)
-    {
-      t[i][j] = 0;
-    }
-  }
+    vector<vector<int>> t(n + 1, vector<int>(W + 1));
 
-  // DP
-  for (int i = 1; i <= n; i++)
-  {
-    for (int j = 1; j <= W; j++)
+    // initialize
+    // n, W ki jgh i and j
+    for (int i = 0; i <= n; i++)
     {
-      if (wt[i - 1] <= j)
+      for (int j = 0; j <= W; j++)
       {
-        t[i][j] = max(
-            val[i - 1] + t[i - 1][j - wt[i - 1]],
-            t[i - 1][j]);
-      }
-      else
-      {
-        t[i][j] = t[i - 1][j];
+        t[i][j] = 0;
       }
     }
-  }
 
-  return t[n][W];
-}
+    // DP
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= W; j++)
+      {
+        if (wt[i - 1] <= j)
+        {
+          t[i][j] = max(
+              val[i - 1] + t[i - 1][j - wt[i - 1]],
+              t[i - 1][j]);
+        }
+        else
+        {
+          t[i][j] = t[i - 1][j];
+        }
+      }
+    }
+
+    return t[n][W];
+  }
+};
 
 int main()
 {
-  int wt[] = {1, 3, 4, 5};
-  int val[] = {1, 4, 5, 7};
-  int W = 7;
-  int n = 4;
+  vector<int> wt = {1, 3, 4, 5};
+  vector<int> val = {1, 4, 5, 7};
 
-  cout << knapsack(wt, val, W, n);
+  int W = 7;
+  int n = wt.size();
+
+  Solution obj;
+
+  cout << obj.knapsack(wt, val, W, n);
+
+  return 0;
 }

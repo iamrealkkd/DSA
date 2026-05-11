f39525843f7f13856d1190abe900c96005d37b5d
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-11 10:59:49 +0530

    LCS Bottom Up Updation

--- 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/10_2_longest_common_subsequence_bottom_up.cpp
+++ 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/10_2_longest_common_subsequence_bottom_up.cpp
@@ -4,31 +4,36 @@ using namespace std;
 class Solution
 {
 public:
-    // Recursive LCS Function
-    int longestCommonSubsequence(string x, string y, int m, int n)
+    int longestCommonSubsequence(string x, string y)
     {
+        int m = x.size();
+        int n = y.size();
+
         vector<vector<int>> t(m + 1, vector<int>(n + 1, -1));
 
         // Initialization
         for (int i = 0; i <= m; i++)
             t[i][0] = 0;
+
         for (int j = 0; j <= n; j++)
             t[0][j] = 0;
+
+        // DP Table Filling
         for (int i = 1; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
+
                 // If characters match
                 if (x[i - 1] == y[j - 1])
                     t[i][j] = 1 + t[i - 1][j - 1];
 
                 // If characters do not match
                 else
-                {
                     t[i][j] = max(t[i][j - 1], t[i - 1][j]);
-                }
             }
         }
+
         return t[m][n];
     }
 };
@@ -36,16 +41,13 @@ public:
 int main()
 {
 
-    // Default values
+    // Default Input
     string text1 = "abcde";
     string text2 = "ace";
 
-    int m = text1.size();
-    int n = text2.size();
-
     Solution obj;
 
-    cout << obj.longestCommonSubsequence(text1, text2, m, n);
+    cout << obj.longestCommonSubsequence(text1, text2);
 
     return 0;
 }

7a57c405ec0f29a647a198bc54ec20274bb2a393
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-14 09:24:51 +0530

    Soln updated

--- 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/14_0_minimum_number_of_deletions_and_insertions.cpp
+++ 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/14_0_minimum_number_of_deletions_and_insertions.cpp
@@ -4,11 +4,12 @@ using namespace std;
 class Solution
 {
 public:
-    int longestCommonSubsequence(string x, string y)
+    int minOperations(string &x, string &y)
     {
         int m = x.size();
         int n = y.size();
 
+        // DP table for LCS
         vector<vector<int>> t(m + 1, vector<int>(n + 1, -1));
 
         // Initialization
@@ -18,40 +19,41 @@ public:
         for (int j = 0; j <= n; j++)
             t[0][j] = 0;
 
-        // DP Table Filling
+        // Filling DP table
         for (int i = 1; i <= m; i++)
         {
             for (int j = 1; j <= n; j++)
             {
-
                 // If characters match
                 if (x[i - 1] == y[j - 1])
                     t[i][j] = 1 + t[i - 1][j - 1];
 
                 // If characters do not match
                 else
-                    t[i][j] = max(t[i][j - 1], t[i - 1][j]);
+                    t[i][j] = max(t[i - 1][j], t[i][j - 1]);
             }
         }
 
+        // Length of LCS
         int k = t[m][n];
+
+        // Operations
         int deletion = m - k;
         int insertion = n - k;
-        int tot = deletion + insertion;
-        return tot;
+
+        return deletion + insertion;
     }
 };
 
 int main()
 {
 
-    // Default Input
-    string text1 = "geeksforgeeks";
-    string text2 = "geeks";
+    string s1 = "geeksforgeeks";
+    string s2 = "geeks";
 
     Solution obj;
 
-    cout << obj.longestCommonSubsequence(text1, text2);
+    cout << obj.minOperations(s1, s2);
 
     return 0;
 }

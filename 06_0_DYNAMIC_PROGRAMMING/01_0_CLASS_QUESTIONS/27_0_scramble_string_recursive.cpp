5fa4a2801bcb496f55d44105c879bbb8212f2e0b
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-18 15:00:59 +0530

    SCRAMBLE STRING

---
+++ 06_0_DYNAMIC_PROGRAMMING/01_0_CLASS_QUESTIONS/27_0_scramble_string_recursive.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(string a, string b)
+{
+    // Base cases
+    if(a == b)
+        return true;
+
+    if(a.length() <= 1)
+        return false;
+
+    int n = a.length();
+
+    for(int i = 1; i <= n - 1; i++)
+    {
+        // Swap case
+        bool cond1 =
+            solve(a.substr(0, i), b.substr(n - i, i)) &&
+            solve(a.substr(i, n - i), b.substr(0, n - i));
+
+        // No swap case
+        bool cond2 =
+            solve(a.substr(0, i), b.substr(0, i)) &&
+            solve(a.substr(i, n - i), b.substr(i, n - i));
+
+        if(cond1 || cond2)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    string s1 = "great";
+    string s2 = "rgeat";
+
+    if(solve(s1, s2))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}

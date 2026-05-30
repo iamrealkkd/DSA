1f30996b70c8fdca8bef0aff6e881f883b197093
iamrealkkd <kkrishna.bga@gmail.com>
2026-05-30 18:20:24 +0530

    Level Order Traversal Updated

--- 08_0_TREES/01_0_CLASS_QUESTIONS/02_0_preorder_inorder_postorder_levelorder_traversal.cpp
+++ 08_0_TREES/01_0_CLASS_QUESTIONS/02_0_preorder_inorder_postorder_levelorder_traversal.cpp
@@ -60,10 +60,21 @@ cout << root -> data << " ";
 void levelOrder(Node* root){
 queue<Node*> q;
 q.push(root);
+q.push(NULL);
 
 while(q.size() > 0){
 Node* curr = q.front();
 q.pop();
+
+if(curr == NULL){
+    if(!q.empty()){
+     cout << endl;
+     q.push(NULL);
+     continue;
+    }else{
+       break;
+    }
+}
 
 cout << curr -> data << " ";
 

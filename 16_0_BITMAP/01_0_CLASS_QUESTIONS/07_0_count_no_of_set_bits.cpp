09a82ed9b200d800717cc0877b5c0705f353831c
iamrealkkd <kkrishna.bga@gmail.com>
2026-06-13 15:37:30 +0530

    Hack Added

--- 16_0_BITMAP/01_0_CLASS_QUESTIONS/07_0_count_no_of_set_bits.cpp
+++ 16_0_BITMAP/01_0_CLASS_QUESTIONS/07_0_count_no_of_set_bits.cpp
@@ -15,12 +15,26 @@ int count_bits(int n){
     return count;
 }
 
+//Counting Set Bits : Hack (Faster method)
+int count_bits_hack(int n){
+
+    int ans = 0;
+    while(n>0){
+        //removes the last set bit from the current number
+        n = n & (n-1);
+        ans++;
+    }
+
+    return ans;
+}
+
 int main(){
 
     int n;
     cin>>n;
 
     cout<<count_bits(n) <<endl;
+    cout<<count_bits_hack(n) <<endl;
 
     return 0;
 }

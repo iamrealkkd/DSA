64ca5475341562dcc44831bcd382416d3c07cc78
iamrealkkd <kkrishna.bga@gmail.com>
2026-06-14 21:31:43 +0530

    Reverse Word in a Given String

---
+++ 11_0_GREEDY/01_0_CLASS_QUESTIONS/08_0_reverse_word_in_a_given_string.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string S) {
+        string ans  = "";
+        string temp = "";
+
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if (S[i] == '.') {
+                reverse(temp.begin(), temp.end());
+                ans  = ans + temp + '.';  // <-- add '.' back
+                temp = "";
+            } else {
+                temp.push_back(S[i]);
+            }
+        }
+
+        // last word — no dot after it
+        reverse(temp.begin(), temp.end());
+        ans = ans + temp;
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    cout << obj.reverseWords("i.like.this.program.very.much") << endl;
+    // Expected: much.very.program.this.like.i
+
+    cout << obj.reverseWords("pqr.mno") << endl;
+    // Expected: mno.pqr
+
+    return 0;
+}

ccee20b96179da7f74c27fe8acdf4b8a529cb0c5
iamrealkkd <kkrishna.bga@gmail.com>
2026-06-17 10:41:45 +0530

    Middle of the Linked List

---
+++ 13_0_LINKED_LIST/01_0_CLASS_QUESTIONS/07_0_876_middle_of_the_linked_list..cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = slow;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+// Function to print linked list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val;
+        if (head->next != NULL)
+            cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution obj;
+    ListNode* middle = obj.middleNode(head);
+
+    cout << middle->val << endl;
+
+    return 0;
+}

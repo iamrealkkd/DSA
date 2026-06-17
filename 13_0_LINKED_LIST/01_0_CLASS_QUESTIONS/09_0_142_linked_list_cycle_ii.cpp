7b7129ee6a0ab1a7593be89519326c6dd44b717f
iamrealkkd <kkrishna.bga@gmail.com>
2026-06-17 12:45:44 +0530

    Cycle Detection @

---
+++ 13_0_LINKED_LIST/01_0_CLASS_QUESTIONS/09_0_142_linked_list_cycle_ii.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+// LeetCode Definition
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        bool isCycle = false;
+
+        // Detect cycle
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                isCycle = true;
+                break;
+            }
+        }
+
+        if (!isCycle) {
+            return NULL;
+        }
+
+        // Find start of cycle
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+
+    // Creating linked list:
+    // 3 -> 2 -> 0 -> -4
+    ListNode* head = new ListNode(3);
+    ListNode* second = new ListNode(2);
+    ListNode* third = new ListNode(0);
+    ListNode* fourth = new ListNode(-4);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+
+    // Create cycle
+    // -4 -> 2
+    fourth->next = second;
+
+    Solution obj;
+
+    ListNode* cycleStart = obj.detectCycle(head);
+
+    if (cycleStart != NULL) {
+        cout << "Cycle starts at node with value: "
+             << cycleStart->val << endl;
+    } else {
+        cout << "No Cycle Found" << endl;
+    }
+
+    return 0;
+}

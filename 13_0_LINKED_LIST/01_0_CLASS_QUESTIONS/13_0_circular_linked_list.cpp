0a3df5d9187b730363880919bd2d80a4acf160a6
iamrealkkd <kkrishna.bga@gmail.com>
2026-06-18 11:13:43 +0530

    Code Updated

--- 13_0_LINKED_LIST/01_0_CLASS_QUESTIONS/13_0_circular_linked_list.cpp
+++ 13_0_LINKED_LIST/01_0_CLASS_QUESTIONS/13_0_circular_linked_list.cpp
@@ -47,29 +47,65 @@ public:
         }
     }
 
+    void deleteAtHead() {
+        if (head == NULL) return;
+        else if (head == tail) {  // single node
+            delete head;
+            head = tail = NULL;
+        } else {  // 2 or more
+            Node* temp = head;
+            head = head->next;
+            tail->next = head;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+
+    void deleteAtTail() {
+        if (head == NULL) return;
+        else if (head == tail) {  // single node
+            delete tail;
+            head = tail = NULL;
+        } else {  // 2 or more
+            Node* temp = head;
+            while (temp->next != tail) {
+                temp = temp->next;
+            }
+            temp->next = head;
+            delete tail;
+            tail = temp;
+        }
+    }
+
     void print() {
-    if (head == NULL) return;
+        if (head == NULL) return;
 
-    Node* temp = head;
-    do {
-        cout << temp->data << "->";
-        temp = temp->next;
-    } while (temp != head);
+        Node* temp = head;
+        do {
+            cout << temp->data << "->";
+            temp = temp->next;
+        } while (temp != head);
 
-    cout << head->data << "(head)" << endl;
-}
+        cout << head->data << "(head)" << endl;
+    }
 };
 
 int main() {
     CircularList cll;
 
-    cll.insertAtHead(1);
-    cll.insertAtHead(2);
-
     cll.insertAtTail(1);
     cll.insertAtTail(2);
     cll.insertAtTail(3);
 
+    cout << "Original: ";
+    cll.print();
+
+    cll.deleteAtHead();
+    cout << "After deleteAtHead: ";
+    cll.print();
+
+    cll.deleteAtTail();
+    cout << "After deleteAtTail: ";
     cll.print();
 
     return 0;

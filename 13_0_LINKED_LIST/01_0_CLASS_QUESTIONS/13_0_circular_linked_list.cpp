3eceb96fbdd824d415fb4a7b8560189eeebfe7dd
iamrealkkd <kkrishna.bga@gmail.com>
2026-06-18 10:36:32 +0530

    Circular Linked List

---
+++ 13_0_LINKED_LIST/01_0_CLASS_QUESTIONS/13_0_circular_linked_list.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class CircularList {
+    Node* head;
+    Node* tail;
+
+public:
+    CircularList() {
+        head = tail = NULL;
+    }
+
+    void insertAtHead(int val) {
+        Node* newNode = new Node(val);
+
+        if (tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode->next = head;
+            head = newNode;
+            tail->next = head;
+        }
+    }
+
+    void insertAtTail(int val) {
+        Node* newNode = new Node(val);
+
+        if (tail == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+    }
+
+    void print() {
+        if (head == NULL) return;
+
+        Node* temp = head;
+        do {
+            cout << temp->data;
+            temp = temp->next;
+            if (temp != head) cout << "->";
+        } while (temp != head);
+        cout << endl;
+    }
+};
+
+int main() {
+    CircularList cll;
+
+    cll.insertAtHead(1);
+    cll.insertAtHead(2);
+
+    cll.insertAtTail(1);
+    cll.insertAtTail(2);
+    cll.insertAtTail(3);
+
+    cll.print();
+
+    return 0;
+}

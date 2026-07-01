0b1a273570e34ee859aebff8f2e87f44d3c9731d
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-07-01 20:48:23 +0530

    Folder Updation

---
+++ 02_0_ARRAY/03_0_PATTERN_BASED_QUESTIONS/01_0_TWO_POINTERS/01_0_CONVERGING/02_0_15_3sum.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        set<vector<int>> st;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while (l < r) {
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    st.insert({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                }
+                else if (sum < 0) {
+                    l++;
+                }
+                else {
+                    r--;
+                }
+            }
+        }
+
+        vector<vector<int>> ans(st.begin(), st.end());
+
+        return ans;
+    }
+};
+
+int main() {
+
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    Solution obj;
+
+    vector<vector<int>> ans = obj.threeSum(nums);
+
+    cout << "Triplets whose sum is 0:\n";
+
+    if (ans.empty()) {
+        cout << "No triplets found.\n";
+    }
+    else {
+
+        cout << "[";
+
+        for (int i = 0; i < ans.size(); i++) {
+
+            cout << "[";
+
+            for (int j = 0; j < ans[i].size(); j++) {
+
+                cout << ans[i][j];
+
+                if (j != ans[i].size() - 1)
+                    cout << ",";
+            }
+
+            cout << "]";
+
+            if (i != ans.size() - 1)
+                cout << ",";
+        }
+
+        cout << "]" << endl;
+    }
+
+    return 0;
+}

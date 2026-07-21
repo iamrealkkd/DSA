611e4cee3081cdc688b7fbe424d11fe798c079a7
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-07-21 19:26:35 +0530

    Adjancency List Code Corrected

--- 09_0_GRAPHS/01_0_CLASS_QUESTIONS/02_0_undirected_adjacency_list.cpp
+++ 09_0_GRAPHS/01_0_CLASS_QUESTIONS/02_0_undirected_adjacency_list.cpp
@@ -1,46 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_graph(vector<vector<int>> adjacencyMatrix) {
-    for (int i = 1; i <= 4; i++) {
-        cout << "Node: " << i << ", Neighbors: ";
+void print_graph(vector<vector<int>> &adjacencyList, int V) {
 
-        for (int j = 1; j <= 4; j++) {
-            if (adjacencyMatrix[i][j] == 1) {
-                cout << j << " ";
-            }
-        }
+  for (int i = 1; i <= V; i++) {
 
-        cout << endl;
+    cout << "Node: " << i << ", Neighbors: ";
+
+    for (int j = 0; j < adjacencyList[i].size(); j++) {
+      cout << adjacencyList[i][j] << " ";
     }
+
+    cout << endl;
+  }
 }
 
 int main() {
 
-    vector<vector<int>> edgeList = {
-        {1, 2},
-        {2, 3},
-        {3, 4},
-        {4, 2},
-        {1, 3}
-    };
+  vector<vector<int>> edgeList = {{1, 2}, {2, 3}, {3, 4}, {4, 2}, {1, 3}};
 
-    int V = 4;
+  int V = 4;
 
-    vector<vector<int>> adjacencyMatrix(V + 1,
-                                        vector<int>(V + 1, 0));
+  // Har node ke liye neighbors ki ek list
+  vector<vector<int>> adjacencyList(V + 1);
 
-    for (int i = 0; i < edgeList.size(); i++) {
+  for (int i = 0; i < edgeList.size(); i++) {
 
-        // Undirected graph
-        int a = edgeList[i][0];
-        int b = edgeList[i][1];
+    int a = edgeList[i][0];
+    int b = edgeList[i][1];
 
-        adjacencyMatrix[a][b] = 1;
-        adjacencyMatrix[b][a] = 1;
-    }
+    // Undirected graph
+    adjacencyList[a].push_back(b);
+    adjacencyList[b].push_back(a);
+  }
 
-    print_graph(adjacencyMatrix);
+  print_graph(adjacencyList, V);
 
-    return 0;
+  return 0;
 }

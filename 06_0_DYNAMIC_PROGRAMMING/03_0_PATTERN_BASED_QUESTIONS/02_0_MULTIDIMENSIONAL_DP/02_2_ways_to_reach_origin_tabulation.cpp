eb89f45fc7636f5fcff7f7e179886aab62cd17d2
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-18 12:23:54 +0530

    Upppppppp

--- 06_0_DYNAMIC_PROGRAMMING/03_0_PATTERN_BASED_QUESTIONS/02_0_MULTIDIMENSIONAL_DP/02_2_ways_to_reach_origin_tabulation.cpp
+++ 06_0_DYNAMIC_PROGRAMMING/03_0_PATTERN_BASED_QUESTIONS/02_0_MULTIDIMENSIONAL_DP/02_2_ways_to_reach_origin_tabulation.cpp
@@ -9,10 +9,10 @@ int ways(int x, int y) {
 
   // dp[0][0] = 1;
 
-  for (int i = 1; i <= x; i++)
+  for (int i = 0; i <= x; i++)
     dp[i][0] = 1;
 
-  for (int j = 1; j <= y; j++)
+  for (int j = 0; j <= y; j++)
     dp[0][j] = 1;
 
   for (int i = 1; i <= x; i++) {
